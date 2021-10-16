01ece5d3124b31896e31ce69c9fe4753b2932eb7
Isabella Riquetti <riquetti.isabella@gmail.com>
2021-10-16 20:04:33 -0300

    Chapter 1 - Question 3

---
+++ CrackingTheCodingInterview/Chapter01ArraysAndStrings/Question03/Chaper01Question03.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace CrackingTheCodingInterview.Chapter1ArraysAndStrings.Question1
+{
+    public static class Chaper01Question03
+    {
+        //Time O(n), Space O(1)
+        public static string Solution1(string text)
+        {
+            StringBuilder strBuilder = new StringBuilder();
+            bool started = false;
+            string currentConcat = ""; // To avoid adding spaces at the end
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] != ' ')
+                {
+                    started = true;
+                    strBuilder.Append(currentConcat);
+                    strBuilder.Append(text[i]);
+                    currentConcat = "";
+                }
+                else if (started)
+                {
+                    currentConcat += "%20";
+                }
+            }
+
+            return strBuilder.ToString();
+        }
+
+        //Mixed solution, Time O(n), Space O(1)
+        public static string Solution2(string text)
+        {
+            text = text.Trim();
+            string[] newStr = new string[text.Length];
+            for (int i = 0; i < text.Length; i++)
+                newStr[i] = text[i] == ' ' ? "%20" : text[i].ToString();
+
+            return string.Join("", newStr);
+        }
+
+        //Mixed solution, Time O(n), Space O(1)
+        public static string Solution3(string text)
+        {
+            StringBuilder strBuilder = new StringBuilder();
+            text = text.Trim();
+            for (int i = 0; i < text.Length; i++)
+                strBuilder.Append(text[i] == ' ' ? "%20" : text[i]);
+
+            return strBuilder.ToString();
+        }
+
+        //Using string lib, Time O(1), Space O(1)
+        public static string Solution4(string text)
+        {
+            return text.Trim().Replace(" ", "%20");
+        }
+
+        //Using string lib, Time O(1), Space O(1)
+        public static string Solution5(string text)
+        {
+            StringBuilder strBuilder = new StringBuilder(text.Trim());
+            strBuilder.Replace(" ", "%20");
+            return strBuilder.ToString();
+        }
+    }
+}
