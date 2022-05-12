fc6890654caf897d6598f45206c184d7fef32726
Isabella Riquetti <riquetti.isabella@gmail.com>
2022-05-11 23:31:03 -0300

    CCI - Chapter 1 - Question 5

--- CrackingTheCodingInterview/Chapter01ArraysAndStrings/Question04/Chaper01Question04.cs
+++ CrackingTheCodingInterview/Chapter01ArraysAndStrings/Question04/Chaper01Question04.cs
@@ -5,6 +5,8 @@ namespace CrackingTheCodingInterview.Chapter01ArraysAndStrings.Question04
 {
     public static class Chaper01Question04
     {
+        // Time: O(n)
+        // Space: O(n)
         public static bool Solution1(string text)
         {
             var hashSet = new HashSet<char>();
