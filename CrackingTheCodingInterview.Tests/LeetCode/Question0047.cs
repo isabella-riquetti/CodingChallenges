870453078c430f253a403c3762fa8cb22fb27631
Isabella Riquetti <riquetti.isabella@gmail.com>
2022-05-11 23:31:17 -0300

    LeetCode - Discord Daily Challange (47)

---
+++ CrackingTheCodingInterview.Tests/LeetCode/Question0047.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+using FluentAssertions;
+
+namespace CrakingTheCodeInterview.Test.LeetCode
+{
+    public class Question0047
+    {
+        [Theory]
+        [MemberData(nameof(PermutationsNoRepetitionsTests))]
+        public void Solution1(PermutationsNoRepetitionsTestInput test)
+        {
+            var result = CrackingTheCodingInterview.LeetCode.Question0047.Resolution.Solution1(test.Value);
+
+            test.ExpecteResult.Should().BeEquivalentTo(result);
+        }
+
+        public static TheoryData<PermutationsNoRepetitionsTestInput> PermutationsNoRepetitionsTests = new TheoryData<PermutationsNoRepetitionsTestInput>
+        {
+            new PermutationsNoRepetitionsTestInput()
+            {
+                Name = "121",
+                Value = new int[] { 1,2,1 },
+                ExpecteResult = new List<IList<int>>()
+                {
+                    new List<int>() { 1,1,2 },
+                    new List<int>() { 1,2,1 },
+                    new List<int>() { 2,1,1 },
+                }
+            },
+            new PermutationsNoRepetitionsTestInput()
+            {
+                Name = "111",
+                Value = new int[] { 1,2,1 },
+                ExpecteResult = new List<IList<int>>()
+                {
+                    new List<int>() { 1,1,2 },
+                    new List<int>() { 1,2,1 },
+                    new List<int>() { 2,1,1 },
+                }
+            },
+            new PermutationsNoRepetitionsTestInput()
+            {
+                Name = "123",
+                Value = new int[] { 1,2,3 },
+                ExpecteResult = new List<IList<int>>()
+                {
+                    new List<int>() { 1,2,3 },
+                    new List<int>() { 1,3,2 },
+                    new List<int>() { 2,1,3 },
+                    new List<int>() { 2,3,1 },
+                    new List<int>() { 3,1,2 },
+                    new List<int>() { 3,2,1 },
+                }
+            }
+        };
+
+        [ExcludeFromCodeCoverage]
+        public class PermutationsNoRepetitionsTestInput
+        {
+            public string Name { get; set; }
+            public int[] Value { get; set; }
+            public IList<IList<int>> ExpecteResult { get; set; }
+        }
+
+    }
+}
---
+++ CrackingTheCodingInterview/LeetCode/Question0047/Resolution.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace CrackingTheCodingInterview.LeetCode.Question0047
+{
+    public static class Resolution
+    {
+        public static IList<IList<int>> Solution1(int[] nums)
+        {
+            var result = new List<IList<int>>();
+            PossibleContinuations(nums, new HashSet<int>(), result, new List<int>());
+            return result;
+        }
+
+        public static void PossibleContinuations(int[] nums, HashSet<int> usedIndexes, IList<IList<int>> result, IList<int> currentList)
+        {
+            var numbersUsedInThePosition = new HashSet<int>(nums.Length - usedIndexes.Count);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (usedIndexes.Contains(i) || numbersUsedInThePosition.Contains(nums[i]))
+                {
+                    continue;
+                }
+                else
+                {
+                    var newList = new List<int>(currentList);
+                    var newUsedIndexes = new HashSet<int>(usedIndexes);
+                    numbersUsedInThePosition.Add(nums[i]);
+                    newList.Add(nums[i]);
+                    newUsedIndexes.Add(i);
+                    if (newList.Count == nums.Length)
+                    {
+                        result.Add(newList);
+                    }
+                    else
+                    {
+                        PossibleContinuations(nums, newUsedIndexes, result, newList);
+                    }
+                }
+            }
+        }
+    }
+}
