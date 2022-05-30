20c31f2a54c1cf3e06df8235569e280400bfb5f1
Isabella Riquetti <riquetti.isabella@gmail.com>
2022-05-30 17:50:52 -0300

    LeetCode Google 20

---
+++ CrackingTheCodingInterview.Tests/LeetCode/Question0020.cs
@@ -0,0 +1,84 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace CrakingTheCodeInterview.Test.LeetCode
+{
+    public class Question0020
+    {
+        [Theory]
+        [MemberData(nameof(ParethesisValidationTests))]
+        public void Solution1(ParethesisValidationTestInput test)
+        {
+            var result = CrackingTheCodingInterview.LeetCode.Question0020.Resolution.Solution1(test.Input);
+
+            Assert.Equal(test.ExpecteResult, result);
+        }
+
+        public static TheoryData<ParethesisValidationTestInput> ParethesisValidationTests = new TheoryData<ParethesisValidationTestInput>
+        {
+            new ParethesisValidationTestInput()
+            {
+                Name = "Empty",
+                Input = "",
+                ExpecteResult = true
+            },
+            new ParethesisValidationTestInput()
+            {
+                Name = "Valid single",
+                Input = "[]",
+                ExpecteResult = true
+            },
+            new ParethesisValidationTestInput()
+            {
+                Name = "Valid mix side-by-side",
+                Input = "()[]{}",
+                ExpecteResult = true
+            },
+            new ParethesisValidationTestInput()
+            {
+                Name = "Valid mix in between",
+                Input = "([{}])",
+                ExpecteResult = true
+            },
+            new ParethesisValidationTestInput()
+            {
+                Name = "Valid mix in between and side by side",
+                Input = "([{}])([]){}",
+                ExpecteResult = true
+            },
+            new ParethesisValidationTestInput()
+            {
+                Name = "Invalid closing",
+                Input = ")",
+                ExpecteResult = false
+            },
+            new ParethesisValidationTestInput()
+            {
+                Name = "Invalid missing",
+                Input = "()[",
+                ExpecteResult = false
+            },
+            new ParethesisValidationTestInput()
+            {
+                Name = "Invalid different",
+                Input = "(]",
+                ExpecteResult = false
+            },
+            new ParethesisValidationTestInput()
+            {
+                Name = "Invalid mix",
+                Input = "[(])",
+                ExpecteResult = false
+            },
+        };
+
+        [ExcludeFromCodeCoverage]
+        public class ParethesisValidationTestInput
+        {
+            public string Name { get; set; }
+            public string Input { get; set; }
+            public bool ExpecteResult { get; set; }
+        }
+
+    }
+}
---
+++ CrackingTheCodingInterview/LeetCode/Question0020/Resolution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrackingTheCodingInterview.LeetCode.Question0020
+{
+    public static class Resolution
+    {
+        private static Dictionary<char, char> closes = new Dictionary<char, char>()
+        {
+            {')','(' },
+            {']','[' },
+            {'}','{' }
+        };
+        
+        public static bool Solution1(string parenthesis)
+            {
+            var stack = new Stack<char>();
+            foreach (var item in parenthesis)
+            {
+                if (item == '(' || item == '[' || item == '{')
+                    stack.Push(item);
+                else if (stack.Count == 0)
+                    return false;
+                else
+                {
+                    var closing = closes[item];
+                    var previousCharClose = stack.Pop();
+                    if (previousCharClose != closing)
+                    {
+                        return false;
+                    }
+                }
+            }
+        
+            return stack.Count == 0;
+        }
+    }
+}
