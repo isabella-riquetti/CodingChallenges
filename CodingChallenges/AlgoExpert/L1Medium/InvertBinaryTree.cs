bdf352d9d4126ec1e073d74f8e88b62545af09bd
Isabella Riquetti <riquetti.isabella@gmail.com>
2022-06-05 02:05:46 -0300

    Algo Expert - Invert Binary Tree

---
+++ CodingChallenges/AlgoExpert/L1Medium/InvertBinaryTree.cs
@@ -0,0 +1,34 @@
+﻿namespace CodingChallenges.CrakingTheCodingInterviewAlgoExpert.Medium
+{
+    public static class InvertBinaryTree
+    {
+        public static void Solution1(BinaryTree tree)
+        {
+            Swap(tree);
+        }
+
+        private static void Swap(BinaryTree node)
+        {
+            var previousLeft = node.left;
+            node.left = node.right;
+            node.right = previousLeft;
+
+            if (node.left != null)
+                Swap(node.left);
+            if (node.right != null)
+                Swap(node.right);
+        }
+
+        public class BinaryTree
+        {
+            public int value;
+            public BinaryTree left;
+            public BinaryTree right;
+
+            public BinaryTree(int value)
+            {
+                this.value = value;
+            }
+        }
+    }
+}
